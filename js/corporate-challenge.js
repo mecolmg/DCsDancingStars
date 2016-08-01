ad77ff9ce33f9d0eeea17206295a232dcec4f7bd
mecolmg <mecolmg@vt.edu>
2016-07-31 23:40:07 -0400

    Clicking Images Opens Dialog

--- js/corporate-challenge.js
+++ js/corporate-challenge.js
@@ -57,7 +57,7 @@ function getWidget(fundraiser){
     var template = 
         "<div id='"+id+"' class='mdl-cell mdl-cell--4-col mdl-cell--middle'>" +
            "<div class='demo-card-square mdl-card mdl-shadow--2dp'>" +
-             "<div class='mdl-card__title mdl-card--expand' style='background: linear-gradient(rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.5)),url("+fundraiser.photo.original+") top / cover;'>" +
+             "<div class='mdl-card__title mdl-card--expand' onclick='openViewMore("+id+")' style='background: linear-gradient(rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.5)),url("+fundraiser.photo.original+") top / cover;'>" +
                  "<h2 class='mdl-card__title-text'>"+fundraiser.title+"</h2>" +
              "</div>" +
              "<div class='mdl-card__supporting-text'>"+fundraiser.description.replace(/\n/g,"<br>") +"</div>" +
