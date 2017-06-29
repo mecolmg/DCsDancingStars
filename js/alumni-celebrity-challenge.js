301911ec03a589bbf1ebb1b115d936ebe5e10a00
Colm Gallagher <cgallagher@Colms-MacBook-Pro.local>
2017-06-28 23:09:54 -0400

    updated to 2017 pages and links

--- js/alumni-celebrity-challenge.js
+++ js/alumni-celebrity-challenge.js
@@ -7,7 +7,7 @@ $.ajax({
     dataType: 'json',
     success: function(data) {
         for(var i=0; i<data.fundraisers.length; i++){
-            if(!data.fundraisers[i].archived && data.fundraisers[i].campaign_id == 5327){
+            if(!data.fundraisers[i].archived && data.fundraisers[i].campaign_id == 5324){
                 fundraisers.push(data.fundraisers[i]);
             }
         }
@@ -112,7 +112,7 @@ function openDialog(id){
                 "type='text/javascript' " +
                 "data-donately-address='true' " +
                 "data-donately-id='act_db6f79c68de8' " +
-                "data-donately-campaign-id='5327' " +
+                "data-donately-campaign-id='5324' " +
                 "data-stripe-publishable-key='pk_live_F1qPdCs0uBdkOptbCd35jLUo' " +
                 "data-donately-duration='only_onetime' " +
                 "data-donately-amount='250' " + 
